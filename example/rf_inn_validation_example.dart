1fe6d763c734624e096b23140eda784a4cddf012
Andrey Rudenko <arudenko@corp.finam.ru>
2021-02-11 14:24:50 +0300

    Check organization INN. Readme

--- example/rf_inn_validation_example.dart
+++ example/rf_inn_validation_example.dart
@@ -1,5 +1,9 @@
 import 'package:rf_inn_validation/rf_inn_validation.dart';
 
 void main() {
+  final someOrganizationInn = '7728168971';
+  final somePersonInn = '618620222755';
 
+  print(isOrganizationInnValid(someOrganizationInn));
+  print(isPersonInnValid(somePersonInn));
 }
--- test/rf_inn_validation_test.dart
+++ test/rf_inn_validation_test.dart
@@ -7,7 +7,16 @@ void main() {
     expect(isTwelveDigitsString('abcabcabcabc'), false);
     expect(isTwelveDigitsString('12345678901'), false);
     expect(isTwelveDigitsString('a23456789012'), false);
+    expect(isTwelveDigitsString('1234567890123'), false);
     expect(isTwelveDigitsString('123456789012'), true);
+  });
+
+  test('assure that input is 12 digits string', () {
+    expect(isTenDigitsString(''), false);
+    expect(isTenDigitsString('abcabcabca'), false);
+    expect(isTenDigitsString('a23456789'), false);
+    expect(isTenDigitsString('12345678901'), false);
+    expect(isTenDigitsString('1234567890'), true);
   });
 
   test('test person inn correctness', () {
@@ -20,5 +29,17 @@ void main() {
     // valid
     expect(isPersonInnValid('027188439560'), true);
     expect(isPersonInnValid('618620222755'), true);
+  });
+
+  test('test organization inn correctness', () {
+    // invalid
+    expect(isOrganizationInnValid(null), false);
+    expect(isOrganizationInnValid(''), false);
+    expect(isOrganizationInnValid('1'), false);
+    expect(isOrganizationInnValid('1234567890'), false);
+    expect(isOrganizationInnValid('7723274005'), false);
+    // valid
+    expect(isOrganizationInnValid('7728168971'), true); // alfa bank
+    expect(isOrganizationInnValid('7710140679'), true); // tinkoff bank
   });
 }
